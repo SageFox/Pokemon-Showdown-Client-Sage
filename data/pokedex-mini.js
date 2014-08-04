dc90585cace1d5348f96fc471e9283613fbc440d
SageFox <sagerfox@gmail.com>
2014-08-04 14:06:45 +0200

    Fixed missing "," and falsely placed ","

--- data/pokedex-mini.js
+++ data/pokedex-mini.js
@@ -819,7 +819,7 @@ BattlePokemonSprites = {
 	"xerneas": {num:716, front:{ani:{w:106,h:127}}, back:{ani:{w:94,h:128}}},
 	"yveltal": {num:717, front:{ani:{w:201,h:188}}, back:{ani:{w:186,h:194}}},
 	"zygarde": {num:718, front:{ani:{w:96,h:107}}, back:{ani:{w:113,h:123}}},
-	"diancie": {num:719, front:{ani:{w:50,h:90}}, back:{ani:{w:50,h:90}}}
+	"diancie": {num:719, front:{ani:{w:50,h:90}}, back:{ani:{w:50,h:90}}},
 	
 		
 
@@ -922,7 +922,7 @@ BattlePokemonSprites = {
     "cardinite": {num:1097},
     "chardinal": {num:1098},
     "skurrow": {num:1099},
-    "somberado": {num:1100},
+    "somberado": {num:1100}
 
 
 };
